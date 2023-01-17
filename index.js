f5b4962f002b3b674e459b44d08c1912d58eb42c
Pranaya <patropranaya23@gmail.com>
2023-01-17 23:03:26 +0530

    Close icon is working

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let toggle = document.getElementById("toggle");
 let menu = document.getElementById("navbar");
+let close = document.getElementById("close");
 console.log("Toggle:-",toggle);
 console.log("menu:-",menu);
 toggle.addEventListener('click',function(){
     
     menu.classList.add('active');
     console.log("menu:-",menu);
+});
+
+close.addEventListener('click', function(){
+    menu.classList.remove('active');
 });
